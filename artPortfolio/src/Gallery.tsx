54a2889f0a1601d51f43308534934062ac7a8a67
Jacob <92279567+Oia20@users.noreply.github.com>
2024-04-20 11:45:15 -0500

    Vite config files, added background image, and section components.

---
+++ artPortfolio/src/Gallery.tsx
@@ -0,0 +1,10 @@
+const Gallery = () => {
+    return (
+      <section className="Gallery">
+        <h2>Gallery</h2>
+        {/* Your gallery content goes here */}
+      </section>
+    );
+  };
+  
+  export default Gallery;
