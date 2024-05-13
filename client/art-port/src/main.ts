0f3ca9bc02d094842f207651b1e3d049bf2bcce9
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-12 21:28:33 -0500

    Added new component, learning framework

--- client/art-port/src/main.ts
+++ client/art-port/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './header/header.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HeaderComponent, appConfig)
   .catch((err) => console.error(err));
