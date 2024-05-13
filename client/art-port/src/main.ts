371449d1998f9dead1d4c581279aab1e14f8f977
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-13 13:59:12 -0500

    Working on layout, added header section skeleton.

--- client/art-port/src/main.ts
+++ client/art-port/src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component';
 import { HeaderComponent } from './header/header.component';
 
+
 bootstrapApplication(HeaderComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
