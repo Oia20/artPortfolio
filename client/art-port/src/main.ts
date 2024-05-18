9ae0023fbe831f89bc8416daf43b8baecf3cb5db
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-18 17:32:10 -0500

    Added divider to bottom of about, bootstrapped new component.

--- client/art-port/src/main.ts
+++ client/art-port/src/main.ts
@@ -2,10 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { ProjectsComponent } from './projects/projects.component';
 
 bootstrapApplication(HeaderComponent, appConfig)
   .catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+  bootstrapApplication(ProjectsComponent, appConfig)
   .catch((err) => console.error(err));
