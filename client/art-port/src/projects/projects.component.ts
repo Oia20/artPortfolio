c34f24bebcfadc9f50f88876dfb542395dad50b9
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-18 20:04:04 -0500

    projects section additions.

--- client/art-port/src/projects/projects.component.ts
+++ client/art-port/src/projects/projects.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  artistProjects = [
+    {
+      title: 'Project 1',
+      description: 'Description for project 1',
+      image: './assets/DarDock.jpg',
+      tags: ['Tag1', 'Tag2']
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for project 2',
+      image: './assets/ParkTree.jpg',
+      tags: ['Tag3', 'Tag4']
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for project 2',
+      image: './assets/ParkTree2.jpg',
+      tags: ['Tag3', 'Tag4']
+    },
+    // Add more projects here...
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
