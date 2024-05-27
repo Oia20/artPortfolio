e0dab277c608b820031ad42fca3e98bcbf00d650
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-27 14:18:51 -0500

    Added comments explaining how http client request works.

--- client/art-port/src/app/quote/quote.component.ts
+++ client/art-port/src/app/quote/quote.component.ts
@@ -11,24 +11,43 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './quote.component.scss'
 })
 export class QuoteComponent {
+  // Declaration of two TS string class properties to store quote from api.
   quote: string = '';
   author: string = '';
+
+  // Constructor for Angulars httpclient.
   constructor(private http: HttpClient) {
     this.api(); // Call the API method to fetch the quote on component initialization
   }
 
   api() {
-    this.http.get<{quote: string, author: string}[]>('https://api.api-ninjas.com/v1/quotes?category=art', {
+    // Angular's http get syntax '{quote: string, author: string}' is the expected structure of the request
+    this.http.get<{ quote: string, author: string }[]>('https://api.api-ninjas.com/v1/quotes?category=inspirational', {
+      // header with included api key.
       headers: {
         'X-Api-Key': 'I0FGSHrs0O3REcvgvuH4zQ==OYaU3cLbb1Jzz7pn',
       }
-    }).subscribe(response => {
-      if (response && response.length > 0) {
-        this.quote = response[0].quote; // Assuming the API returns an array of quotes
-        this.author = response[0].author;
+      // Each method returns an RxJS Observable which, when subscribed, sends the request and then emits the results when the server responds.
+      // may be subscribed any number of times and will make a new backend request for each subscription.
+    }).subscribe({
+      // This particular subscribe has an object with 3 steps.
+
+      // next which in this fetch handles the expected response.
+      next: response => {
+        if (response && response.length > 0) {
+          this.quote = response[0].quote;
+          this.author = response[0].author;
+        }
+      },
+
+      // error which handles the error, if there is one.
+      error: error => {
+        console.error('Error fetching quote:', error);
+      },
+
+      // lastly complete for exiting the fetch.
+      complete: () => {
       }
-    }, error => {
-      console.error('Error fetching quote:', error);
     });
   }
 }
