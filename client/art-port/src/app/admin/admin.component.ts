2b31ba3a3f5f16570cb81069a63ec332909f5a7d
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-27 13:57:02 -0500

    Quote component comments.

--- client/art-port/src/app/admin/admin.component.ts
+++ client/art-port/src/app/admin/admin.component.ts
@@ -1,35 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { QuoteComponent } from '../quote/quote.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [QuoteComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 
 export class AdminComponent {
-  quote: string = '';
-  author: string = '';
-  constructor(private http: HttpClient) {
-    this.api(); // Call the API method to fetch the quote on component initialization
-  }
 
-  api() {
-    this.http.get<{quote: string, author: string}[]>('https://api.api-ninjas.com/v1/quotes?category=inspirational', {
-      headers: {
-        'X-Api-Key': 'I0FGSHrs0O3REcvgvuH4zQ==OYaU3cLbb1Jzz7pn',
-      }
-    }).subscribe(response => {
-      if (response && response.length > 0) {
-        this.quote = response[0].quote; // Assuming the API returns an array of quotes
-        this.author = response[0].author;
-        console.log(response)
-      }
-    }, error => {
-      console.error('Error fetching quote:', error);
-    });
-  }
 }
--- client/art-port/src/app/quote/quote.component.ts
+++ client/art-port/src/app/quote/quote.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//Quote is using the api ninja quote api, limit is 10k requests per month, we shouldn't make a dent in that.
 
 @Component({
   selector: 'app-quote',
@@ -8,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrl: './quote.component.scss'
 })
 export class QuoteComponent {
+  quote: string = '';
+  author: string = '';
+  constructor(private http: HttpClient) {
+    this.api(); // Call the API method to fetch the quote on component initialization
+  }
 
+  api() {
+    this.http.get<{quote: string, author: string}[]>('https://api.api-ninjas.com/v1/quotes?category=art', {
+      headers: {
+        'X-Api-Key': 'I0FGSHrs0O3REcvgvuH4zQ==OYaU3cLbb1Jzz7pn',
+      }
+    }).subscribe(response => {
+      if (response && response.length > 0) {
+        this.quote = response[0].quote; // Assuming the API returns an array of quotes
+        this.author = response[0].author;
+      }
+    }, error => {
+      console.error('Error fetching quote:', error);
+    });
+  }
 }
