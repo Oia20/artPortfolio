c2fc1affaa79ea26c98cc2b43f991031e37f33b8
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-28 14:39:51 -0500

    Showing error if invalid login creds now.

--- client/art-port/src/app/login/login.component.ts
+++ client/art-port/src/app/login/login.component.ts
@@ -16,19 +16,46 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   error: string = ''; // Variable to store login error message
+  userData: any; // Define a variable to store user data
 
   constructor(private supabaseService: SupabaseService, private router: Router) {}
+  async fetchUserData() {
+    try {
+      const supabase = this.supabaseService.getSupabaseClient();
+
+      const { data: { user } } = await supabase.auth.getUser();
+      this.userData = user; // Assign user data to userData variable
+      console.log(this.userData.aud)
+      if (this.userData.aud) {
+        return
+      } else {
+        this.router.navigate(['/login']);
+
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      this.router.navigate(['/login']);
+
+    }
+  }
 
   async login() {
     try {
       // Attempt to sign in with Supabase
       await this.supabaseService.signIn(this.email, this.password);
+      await this.fetchUserData();
+
+      if (this.userData.aud) {
+        this.router.navigate(['/admin']);
+      } else {
+        this.error = 'Invalid email or password. Please try again.';
+      }
       // If successful, navigate to admin page
-      this.router.navigate(['/admin']);
     } catch (error) {
       // If login fails, display error message
       console.error('Login error:', error);
       this.error = 'Invalid email or password. Please try again.';
+
     }
   }
 }
