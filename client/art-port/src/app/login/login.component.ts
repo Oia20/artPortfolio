693671bc4cd19cb2fb0cb4d11c46bbd1aafd21f9
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-28 14:31:59 -0500

    Firebase deploy deps, and changes to auth

--- client/art-port/src/app/login/login.component.ts
+++ client/art-port/src/app/login/login.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [NgIf, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  error: string = ''; // Variable to store login error message
 
-  constructor(private supabaseService: SupabaseService) {}
+  constructor(private supabaseService: SupabaseService, private router: Router) {}
 
-  login() {
-    console.log(this.email, this.password)
-    this.supabaseService.signIn(this.email, this.password);
+  async login() {
+    try {
+      // Attempt to sign in with Supabase
+      await this.supabaseService.signIn(this.email, this.password);
+      // If successful, navigate to admin page
+      this.router.navigate(['/admin']);
+    } catch (error) {
+      // If login fails, display error message
+      console.error('Login error:', error);
+      this.error = 'Invalid email or password. Please try again.';
+    }
   }
 }
