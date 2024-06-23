fec6bb3305ab2794f25470e3f193fef8d840c5aa
Jacob <92279567+Oia20@users.noreply.github.com>
2024-06-22 20:08:05 -0500

    dependencies for firebase hosting

--- server/artworks/obj/Debug/net8.0/artworks.AssemblyInfo.cs
+++ server/artworks/obj/Debug/net8.0/artworks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("artworks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63cbdf1243b84224eafe9b190ea97863b158caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140aafd731a6ac565835b997fbb1e5689118e251")]
 [assembly: System.Reflection.AssemblyProductAttribute("artworks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("artworks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
